b4cda38adc102d633513d13bde62e29d9b5e82c8
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-08-17 23:19:14 +0700

    added data parameter in new method in Blockchain impl

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,13 @@ struct Blockchain {
 }
 
 impl Blockchain {
-    pub fn new() -> Blockchain {
+    pub fn new(data: &str) -> Blockchain {
         let index = 0;
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .unwrap()
             .as_secs();
-        let data = String::from("Genesis Block");
+        let data = format!("{data}");
         let previous_hash = String::from("0");
         let hash = calculate_hash(index, timestamp, &data, &previous_hash);
 
@@ -56,6 +56,6 @@ pub fn calculate_hash(index: u64, timestamp: u64, data: &str, previous_hash: &st
 }
 
 fn main() {
-    let gen_block = Blockchain::new();
+    let gen_block = Blockchain::new("Genesis block");
     println!("{:?}", gen_block);
 }
