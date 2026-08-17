8897bbce5d3a5f37a0bc3636bc8241e6b254250d
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-08-17 21:07:41 +0700

    added blockchain

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,18 @@ use std::time::{SystemTime, UNIX_EPOCH};
 
 use sha2::Sha256;
 
+#[derive(Debug)]
 struct Block {
     index: u64,
     timestamp: u64,
     data: String,
     previous_hash: String,
     hash: String,
+}
+
+#[derive(Debug)]
+struct Blockchain {
+    blocks: Vec<Block>,
 }
 
 fn calculate_hash(index: u64, timestamp: u64, data: &str, previous_hash: &str) -> String {
@@ -19,6 +25,7 @@ fn calculate_hash(index: u64, timestamp: u64, data: &str, previous_hash: &str) -
 }
 
 fn main() {
+    // genesis block
     let index = 0;
     let timestamp = SystemTime::now()
         .duration_since(UNIX_EPOCH)
@@ -28,36 +35,34 @@ fn main() {
     let previous_hash = String::from("0");
     let hash = calculate_hash(index, timestamp, &data, &previous_hash);
 
-    let block1 = Block {
+    let genesis_block = Block {
         index: index,
         timestamp: timestamp,
         data: data,
         previous_hash: previous_hash,
         hash: hash,
     };
+    // block 1
+    let block1_index = 1;
+    let block1_timestamp = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let block1_data = String::from("Transfer from genesis block");
+    let genesis_block_hash = genesis_block.hash.clone();
+    let block1_hash = calculate_hash(index, timestamp, &block1_data, &genesis_block_hash);
 
-    let index1 = 0;
-    let data1 = String::from("Transferred from Block 1");
-    let prev_hash1 = block1.hash.clone();
-    let new_hash = calculate_hash(index1, timestamp, &data1, &prev_hash1);
-
-    let block2 = Block {
-        index: index1,
-        timestamp: timestamp,
-        data: data1,
-        previous_hash: prev_hash1,
-        hash: new_hash,
+    let block1 = Block {
+        index: block1_index,
+        timestamp: block1_timestamp,
+        data: block1_data,
+        previous_hash: genesis_block_hash,
+        hash: block1_hash,
     };
 
-    println!("Block #{}", block1.index);
-    println!("Timestamp: {}", block1.timestamp);
-    println!("Data: {}", block1.data);
-    println!("Previous Hash: {}", block1.previous_hash);
-    println!("Hash: {}", block1.hash);
-    println!();
-    println!("Block #{}", block2.index);
-    println!("Timestamp: {}", block2.timestamp);
-    println!("Data: {}", block2.data);
-    println!("Previous Hash: {}", block2.previous_hash);
-    println!("Hash: {}", block2.hash);
+    let block_chain = Blockchain {
+        blocks: vec![genesis_block, block1],
+    };
+
+    println!("{:#?}", block_chain);
 }
