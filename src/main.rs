f827b9389f11e7612bfed2c7fc3f557b13f56eb1
Prom Sereyreaksa <prumsereyreaksa@gmail.com>
2026-08-17 23:12:18 +0700

    added new method to Blockchain impl

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,38 @@ struct Blockchain {
     blocks: Vec<Block>,
 }
 
-fn calculate_hash(index: u64, timestamp: u64, data: &str, previous_hash: &str) -> String {
+impl Blockchain {
+    pub fn new() -> Blockchain {
+        let index = 0;
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+        let data = String::from("Genesis Block");
+        let previous_hash = String::from("0");
+        let hash = calculate_hash(index, timestamp, &data, &previous_hash);
+
+        let genesis_block = Block {
+            index: index,
+            timestamp: timestamp,
+            data: data,
+            previous_hash: previous_hash,
+            hash: hash,
+        };
+
+        //return gen. block
+        //
+        Blockchain {
+            blocks: vec![genesis_block],
+        }
+    }
+    pub fn add_block() {}
+    pub fn is_valid() {}
+
+    pub fn print_chain() {}
+}
+
+pub fn calculate_hash(index: u64, timestamp: u64, data: &str, previous_hash: &str) -> String {
     let mut hasher = Sha256::new();
     let combine = format!("{} {} {} {}", index, timestamp, data, previous_hash);
     hasher.update(combine.as_bytes());
@@ -25,44 +56,6 @@ fn calculate_hash(index: u64, timestamp: u64, data: &str, previous_hash: &str) -
 }
 
 fn main() {
-    // genesis block
-    let index = 0;
-    let timestamp = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    let data = String::from("Genesis Block");
-    let previous_hash = String::from("0");
-    let hash = calculate_hash(index, timestamp, &data, &previous_hash);
-
-    let genesis_block = Block {
-        index: index,
-        timestamp: timestamp,
-        data: data,
-        previous_hash: previous_hash,
-        hash: hash,
-    };
-    // block 1
-    let block1_index = 1;
-    let block1_timestamp = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_secs();
-    let block1_data = String::from("Transfer from genesis block");
-    let genesis_block_hash = genesis_block.hash.clone();
-    let block1_hash = calculate_hash(index, timestamp, &block1_data, &genesis_block_hash);
-
-    let block1 = Block {
-        index: block1_index,
-        timestamp: block1_timestamp,
-        data: block1_data,
-        previous_hash: genesis_block_hash,
-        hash: block1_hash,
-    };
-
-    let block_chain = Blockchain {
-        blocks: vec![genesis_block, block1],
-    };
-
-    println!("{:#?}", block_chain);
+    let gen_block = Blockchain::new();
+    println!("{:?}", gen_block);
 }
